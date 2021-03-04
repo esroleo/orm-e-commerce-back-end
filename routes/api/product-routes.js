3e9af2444d7c6b4ca508574a0bae89ad5291f3ca
esroleo <esroleo@gmail.com>
2021-03-04 13:09:18 -0500

    product find all including the tags completed

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,25 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name'],
+        through: ProductTag,
+        as: 'productTag_tag'
+      }
+    ]
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+
 });
 
 // get one product
