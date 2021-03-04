a10e0ea0c568e47ad7b1681f3b2097ff43d27edb
esroleo <esroleo@gmail.com>
2021-03-04 10:40:25 -0500

    working on category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,53 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
+
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  // find and return all categories with associated products
+  Category.findAll({
+    // include Products
+    include: [
+      {
+        model: Product
+      }
+    ]
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  // find one and return category by its `id` value with associated products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      {
+        model: Product
+      }
+    ]     
+  })
+  .then(categoryData => res.json(categoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbPostData => res.json(dbPostData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
